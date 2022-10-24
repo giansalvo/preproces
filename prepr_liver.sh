d79fad405badca08d8c15328542d9dc2d9c8c570
Giansalvo Gusinu <46819324+giansalvo@users.noreply.github.com>
2022-10-24 14:23:17 +0200

    update list of files

--- prepr_liver.sh
+++ prepr_liver.sh
@@ -49,12 +49,73 @@ mkdir $2/$FILLED_SUBFOLDER
 mkdir $2/$TRIMAP_HC_SUBFOLDER
 
 echo "Copying and renaming clean sample images to $2/$CLEAN_SUBFOLDER..."
-mv $2/"Case 7 image 4.png" $2/$CLEAN_SUBFOLDER/I000.png
-mv $2/"Case 8 image 3.png" $2/$CLEAN_SUBFOLDER/I001.png
+mv $2/"Case 7 image 4.png"  $2/$CLEAN_SUBFOLDER/I000.png
+mv $2/"Case 8 image 3.png"  $2/$CLEAN_SUBFOLDER/I001.png
+mv $2/"Case 10 image 3.png" $2/$CLEAN_SUBFOLDER/I002.png
+mv $2/"Case 10 image 4.png" $2/$CLEAN_SUBFOLDER/I003.png
+mv $2/"Case 12 image 2.png" $2/$CLEAN_SUBFOLDER/I004.png
+mv $2/"Case 13 image 2.png" $2/$CLEAN_SUBFOLDER/I005.png
+mv $2/"Case 14 image 2.png" $2/$CLEAN_SUBFOLDER/I006.png
+mv $2/"Case 16 image 2.png" $2/$CLEAN_SUBFOLDER/I007.png
+mv $2/"Case 17 image 4.png" $2/$CLEAN_SUBFOLDER/I008.png
+mv $2/"Case 17 image 5.png" $2/$CLEAN_SUBFOLDER/I009.png
+mv $2/"Case 20 image 2.png" $2/$CLEAN_SUBFOLDER/I010.png
+mv $2/"Case 22 image 2.png" $2/$CLEAN_SUBFOLDER/I011.png
+mv $2/"Case 28 image 2.png" $2/$CLEAN_SUBFOLDER/I012.png
+mv $2/"Case 32 image 2.png" $2/$CLEAN_SUBFOLDER/I013.png
+mv $2/"Case 34 image 3.png" $2/$CLEAN_SUBFOLDER/I014.png
+mv $2/"Case 35 image 2.png" $2/$CLEAN_SUBFOLDER/I015.png
+mv $2/"Case 35 image 3.png" $2/$CLEAN_SUBFOLDER/I016.png
+mv $2/"Case 35 image 4.png" $2/$CLEAN_SUBFOLDER/I017.png
+mv $2/"Case 36 image 3.png" $2/$CLEAN_SUBFOLDER/I018.png
+mv $2/"Case 37 image 2.png" $2/$CLEAN_SUBFOLDER/I019.png
+mv $2/"Case 38 image 1.png" $2/$CLEAN_SUBFOLDER/I020.png
+mv $2/"Case 38 image 3.png" $2/$CLEAN_SUBFOLDER/I021.png
+mv $2/"Case 40 image 2.png" $2/$CLEAN_SUBFOLDER/I022.png
+mv $2/"Case 43 image 2.png" $2/$CLEAN_SUBFOLDER/I023.png
+mv $2/"Case 43 image 4.png" $2/$CLEAN_SUBFOLDER/I024.png
+mv $2/"Case 46 image 1.png" $2/$CLEAN_SUBFOLDER/I025.png
+mv $2/"Case 46 image 3.png" $2/$CLEAN_SUBFOLDER/I026.png
+mv $2/"Case 47 image 6.png" $2/$CLEAN_SUBFOLDER/I027.png
+mv $2/"Case 50 image 2.png" $2/$CLEAN_SUBFOLDER/I028.png
+mv $2/"Case 50 image 3.png" $2/$CLEAN_SUBFOLDER/I029.png
+mv $2/"Case 51 image 1.png" $2/$CLEAN_SUBFOLDER/I030.png
+mv $2/"Case 52 image 1.png" $2/$CLEAN_SUBFOLDER/I031.png
+
 
 echo "Copying and renaming traced images to $2/$TRACED_SUBFOLDER..."
-mv $2/"Case 7 image 4.1.png" $2/$TRACED_SUBFOLDER/I000.png
-mv $2/"Case 8 image 3.1.png" $2/$TRACED_SUBFOLDER/I001.png
+mv $2/"Case 7 image 4.1.png"  $2/$TRACED_SUBFOLDER/I000.png
+mv $2/"Case 8 image 3.1.png"  $2/$TRACED_SUBFOLDER/I001.png
+mv $2/"Case 10 image 3.1.png" $2/$TRACED_SUBFOLDER/I002.png
+mv $2/"Case 10 image 4.1.png" $2/$TRACED_SUBFOLDER/I003.png
+mv $2/"Case 12 image 2.1.png" $2/$TRACED_SUBFOLDER/I004.png
+mv $2/"Case 13 image 2.1.png" $2/$TRACED_SUBFOLDER/I005.png
+mv $2/"Case 14 image 2.1.png" $2/$TRACED_SUBFOLDER/I006.png
+mv $2/"Case 16 image 2.1.png" $2/$TRACED_SUBFOLDER/I007.png
+mv $2/"Case 17 image 4.1.png" $2/$TRACED_SUBFOLDER/I008.png
+mv $2/"Case 17 image 5.1.png" $2/$TRACED_SUBFOLDER/I009.png
+mv $2/"Case 20 image 2.1.png" $2/$TRACED_SUBFOLDER/I010.png
+mv $2/"Case 22 image 2.1.png" $2/$TRACED_SUBFOLDER/I011.png
+mv $2/"Case 28 image 2.1.png" $2/$TRACED_SUBFOLDER/I012.png
+mv $2/"Case 32 image 2.1.png" $2/$TRACED_SUBFOLDER/I013.png
+mv $2/"Case 34 image 3.1.png" $2/$TRACED_SUBFOLDER/I014.png
+mv $2/"Case 35 image 2.1.png" $2/$TRACED_SUBFOLDER/I015.png
+mv $2/"Case 35 image 3.1.png" $2/$TRACED_SUBFOLDER/I016.png
+mv $2/"Case 35 image 4.1.png" $2/$TRACED_SUBFOLDER/I017.png
+mv $2/"Case 36 image 3.1.png" $2/$TRACED_SUBFOLDER/I018.png
+mv $2/"Case 37 image 2.1.png" $2/$TRACED_SUBFOLDER/I019.png
+mv $2/"Case 38 image 1.1.png" $2/$TRACED_SUBFOLDER/I020.png
+mv $2/"Case 38 image 3.1.png" $2/$TRACED_SUBFOLDER/I021.png
+mv $2/"Case 40 image 2.1.png" $2/$TRACED_SUBFOLDER/I022.png
+mv $2/"Case 43 image 2.1.png" $2/$TRACED_SUBFOLDER/I023.png
+mv $2/"Case 43 image 4.1.png" $2/$TRACED_SUBFOLDER/I024.png
+mv $2/"Case 46 image 1.1.png" $2/$TRACED_SUBFOLDER/I025.png
+mv $2/"Case 46 image 3.1.png" $2/$TRACED_SUBFOLDER/I026.png
+mv $2/"Case 47 image 6.1.png" $2/$TRACED_SUBFOLDER/I027.png
+mv $2/"Case 50 image 2.1.png" $2/$TRACED_SUBFOLDER/I028.png
+mv $2/"Case 50 image 3.1.png" $2/$TRACED_SUBFOLDER/I029.png
+mv $2/"Case 51 image 1.1.png" $2/$TRACED_SUBFOLDER/I030.png
+mv $2/"Case 52 image 1.1.png" $2/$TRACED_SUBFOLDER/I031.png
 
 echo "Convert sample images from .png to .jpg..."
 cd $2/$CLEAN_SUBFOLDER
